903dc6092e45cff53dc903bc9ecad46fdb9a4a1f
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-24 19:46:00 -0400

    detail: add category, delete classifier, use classifier

--- bananasorter/urls.py
+++ bananasorter/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<id>[0-9]+)', views.detail, name='detail'),
     url(r'^profile/', views.profile, name='profile'),
-
 ]
--- bananasorter/views.py
+++ bananasorter/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 from django.contrib.auth.models import User
 from bananasorter.models import Classifier, Category
 from bananasorter.forms import ClassifierForm, CategoryForm
@@ -11,6 +12,28 @@ def detail(request, id):
     classifier = Classifier.objects.get(id=id)
     context['classifier'] = classifier
     context['categories'] = Category.objects.filter(classifier=classifier)
+
+    if request.method == 'POST':
+        if request.POST['name'] == 'Classify this!':
+            new_text = request.POST['new_text']
+            prediction = classifier.predict(new_text)
+            context['new_text'] = new_text
+            context['prediction'] = prediction[0]
+
+        elif request.POST['new_category']:
+            form = CategoryForm(request.POST)
+            print(form, "got to submit")
+            if form.is_valid():
+                print(form, "is valid")
+                cat = form.save(commit=False)
+                cat.classifier = classifier
+                cat.save()
+
+        elif request.POST['delete']:
+            print('delete it')
+            classifier.delete()
+            return HttpResponseRedirect('/profile/')
+
     context['classifier_form'] = ClassifierForm()
     context['category_form'] = CategoryForm()
     return render(request, 'bananasorter/detail.html', context)
