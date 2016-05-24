d9e6c37a878f08b421599796e1b488ac6219a123
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-24 16:50:15 -0400

    model forms made

--- bananasorter/views.py
+++ bananasorter/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 def index(request):
     return render('bananasorter/index.html')
 
-def detail(request):
-    return render('bananasorter/detail.html')
+def detail(request, id):
+    context = {}
+    context['classifier'] = Classifier.objects.get(id=id)
+    return render(request, 'bananasorter/detail.html', context)
 
 
 def profile(request):
