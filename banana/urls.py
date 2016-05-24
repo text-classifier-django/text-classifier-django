3cc29eb69d316fdd4ed6758950288ce06cc8b75d
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-24 13:54:20 -0400

    urls and views

--- banana/urls.py
+++ banana/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'banana.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('bananasorter.urls')),
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
