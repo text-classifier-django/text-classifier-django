a7ed824ea43a81aab9c7a31830009ff74ca7c1bb
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-24 17:12:30 -0400

    deleted extra / in urls, more profile view context, database url config fix

--- bananasorter/urls.py
+++ bananasorter/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^', views.index, name='index'),
-    url(r'^/(?P<id>[0-9]+)', views.detail, name='detail'),
+    url(r'^(?P<id>[0-9]+)', views.detail, name='detail'),
     url(r'^profile/', views.profile, name='profile'),
 
 ]
--- bananasorter/views.py
+++ bananasorter/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from bananasorter.models import Classifier, Category
+from bananasorter.forms import ClassifierForm, CategoryForm
 
 def index(request):
     return render('bananasorter/index.html')
 
 def detail(request, id):
     context = {}
-    context['classifier'] = Classifier.objects.get(id=id)
+    classifier = Classifier.objects.get(id=id)
+    context['classifier'] = classifier
+    context['categories'] = Category.objects.filter(classifier=classifier)
+    context['classifier_form'] = ClassifierForm()
+    context['category_form'] = CategoryForm()
     return render(request, 'bananasorter/detail.html', context)
 
 
