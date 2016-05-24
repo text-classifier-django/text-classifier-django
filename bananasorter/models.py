4b15b7254947c4898f6535935212716f84a8f4a6
Julio Cardoza <julio_jr3@hotmail.com>
2016-05-24 11:53:14 -0400

    Made basic models

--- bananasorter/models.py
+++ bananasorter/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Classfier(models.Model):
+    name = models.CharField(max_length=120)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def predict(self, new_line):
+        pipeline = Pipeline([
+            ('count_vect', CountVectorizer()),
+            ('tfidf_trans', TfidfTransformer()),
+            ('multi_nb', MultinomialNB())
+        ])
+
+        x = []
+        y = []
+
+        for cat in Category.objects.filter(classifier=self):
+            for line in cat.training_data:
+                x.append(line)
+                y.append(cat.name)
+
+        pipeline.fit(x, y)
+        return pipeline.predict(new_line)
+
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+    training_data = models.TextField()
+    classifier = models.ForeignKey("Classifier", on_delete=models.CASCADE)
