6db739b5dcdc8465fb17f2b9f67aa89b741bda7a
bijjr <billiejefferys@gmail.com>
2016-05-25 11:24:09 -0400

    Login/Logout feature

--- bananasorter/views.py
+++ bananasorter/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 from bananasorter.models import Classifier, Category, User
 from bananasorter.forms import ClassifierForm, CategoryForm
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+# @login_required(login_url="login/")
 def index(request):
 
     context = {}
