49bc19088047f500f6d77a496bb65fb1ff7ddc18
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-26 11:03:19 -0400

    jqeury complete on detail page, removed more profiledetail links

--- bananasorter/urls.py
+++ bananasorter/urls.py
@@ -6,6 +6,6 @@ app_name = 'bananasorter'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^(?P<id>[0-9]+)', views.detail, name='detail'),
+    url(r'^profile/', views.profile, name='profile'),
 ]
